728f890cdf8b826cba9db0a90399981b4debcecf
yasu_yamasaki <yasuyuki.yamasaki@finatext.com>
2021-05-28 09:13:36 +0900

    create back

---
+++ back/go.mod
@@ -0,0 +1,14 @@
+module github.com/yasu-yamasaki/swagger-sample/back
+
+go 1.16
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/mattn/go-colorable v0.1.8 // indirect
+	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20210513164829-c07d793c2f9a // indirect
+	golang.org/x/net v0.0.0-20210525063256-abc453219eb5 // indirect
+	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
+)
---
+++ back/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"github.com/labstack/echo"
+	"net/http"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/v1/pets", show)
+	e.PUT("/v1/pets", put)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+type Pet struct {
+	Id   string `json:"id"`
+	Name string `json:"name"`
+}
+
+var pets = map[string]Pet{
+	"0": {
+		Id:   "0",
+		Name: "John",
+	},
+}
+
+func show(c echo.Context) error {
+	var res []Pet
+	for _, v := range pets {
+		res = append(res, v)
+	}
+	return c.JSON(http.StatusOK, res)
+}
+
+func put(c echo.Context) error {
+	pet := new(Pet)
+	if err := c.Bind(pet); err != nil {
+		return err
+	}
+	pets[pet.Id] = *pet
+	return c.NoContent(http.StatusOK)
+}
